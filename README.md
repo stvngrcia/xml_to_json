f17399df7c5af437ebdc426cb832164ffd369bce
stvngrcia <stevengarciaacuna@gmail.com>
2016-09-10 13:04:19 -0700

    changed readme file

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # XML TO JSON
-- This class takes the name of an xml file. Then it creates or opens a  
-    file with the same name, but with the extension .json where the results  
-    will be stored. It first converts the xml file to a dictionary and  
-    converts the dictionary in to a json.  
+- This class takes the name of an XML file as an argument passed by the command line.  
+  Then it creates or opens a file with the same name, but with the extension    
+  .json where the results will be stored. It first converts the xml file to a dictionary and  
+  converts the dictionary in to a JSON.  
 - Sources:  
 https://github.com/martinblech/xmltodict  
 https://pythonadventures.wordpress.com/2014/12/29/xml-to-dict-xml-to-json/  
--- xml_to_json.py
+++ xml_to_json.py
@@ -2,10 +2,10 @@ import json
 import sys
 import xmltodict
 """
-    - This class takes the name of an xml file. Then it creates or opens a
-        file with the same name, but with the extension .json where the results
-        will be stored. It first converts the xml file to a dictionary and
-        converts the dictionary in to a json.
+    - This class takes the name of an XML file as an argument passed by the
+    command line. Then it creates or opens a file with the same name, but with
+    the extension.JSON where the results will be stored. It first converts the
+    xml file to a dictionary and converts the dictionary in to a JSON.
     - Sources:
     https://github.com/martinblech/xmltodict
     https://pythonadventures.wordpress.com/2014/12/29/xml-to-dict-xml-to-json/
